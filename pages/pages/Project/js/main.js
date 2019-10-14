bfe5ee05776839e711506bcc6c777911b7b8bd67
Willy8-Dev <56260490+Willy8-Dev@users.noreply.github.com>
2019-10-14 10:39:40 +0200

    Update main.js

--- pages/pages/Project/js/main.js
+++ pages/pages/Project/js/main.js
@@ -8,3 +8,17 @@ function toggleDarkMode(){
         x.style.backgroundColor = "#353535"
     }
 }
+
+function toggleMobileNav() {
+    var x = document.getElementById("nav-mobile");
+    if (x.style.overflow === "visible") {
+      x.style.overflow = "hidden";
+    } else {
+      x.style.overflow = "visible";
+    }
+}
+  
+function hideMobileNav() {
+    var y = document.getElementById("nav-mobile");
+    y.style.overflow = "hidden";
+}
