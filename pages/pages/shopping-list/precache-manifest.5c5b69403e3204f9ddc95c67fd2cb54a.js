67da5dbea96abe8b683b69dfbf93d40bbb09c7d5
Willy8-Dev <56260490+Willy8-Dev@users.noreply.github.com>
2020-02-06 15:00:56 +0100

    Add files via upload

---
+++ pages/pages/shopping-list/precache-manifest.5c5b69403e3204f9ddc95c67fd2cb54a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b3c1777f4ef69fb3ca2952ec7cde90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c3cf24c10b31d5aff01",
+    "url": "./static/css/main.4cf5e0d9.chunk.css"
+  },
+  {
+    "revision": "8a98177d8802277c782f",
+    "url": "./static/js/2.368a1f42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.368a1f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3cf24c10b31d5aff01",
+    "url": "./static/js/main.c8e1c32c.chunk.js"
+  },
+  {
+    "revision": "8d97a0c7da223e073b0b",
+    "url": "./static/js/runtime-main.48d9fc22.js"
+  }
+]);
