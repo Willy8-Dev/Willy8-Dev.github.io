b5d47c9395688a186bcc4ca46a5696276dd63657
Willy8-Dev <56260490+Willy8-Dev@users.noreply.github.com>
2020-05-23 15:58:34 +0200

    github react build

---
+++ build/precache-manifest.02f562a690f5b4cca3832d82efcd76a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63e32f70c590f7e8a550cdf6aba7dfd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d00cd805ecbb74e4a412",
+    "url": "./static/css/main.be3cc12b.chunk.css"
+  },
+  {
+    "revision": "5b9e927ae7c0621b7af2",
+    "url": "./static/js/2.9be538ab.chunk.js"
+  },
+  {
+    "revision": "c6158cf19510b722a48976c8cf21cce4",
+    "url": "./static/js/2.9be538ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00cd805ecbb74e4a412",
+    "url": "./static/js/main.daae7c64.chunk.js"
+  },
+  {
+    "revision": "209bdbf8dd2578881814",
+    "url": "./static/js/runtime-main.d7e5699b.js"
+  },
+  {
+    "revision": "884e83354a16213c21d046188dfad1e8",
+    "url": "./static/media/wood-1350175_1920.884e8335.jpg"
+  }
+]);
